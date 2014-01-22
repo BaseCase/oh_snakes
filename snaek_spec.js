39504d854fb3666317fa952b15c69dd415c8915f
Casey Brant <casey.brant@gmail.com>
2014-01-21 21:53:59 -0800

    extract Snake class; add some unit tests

--- snaek_spec.js
+++ snaek_spec.js
@@ -1,10 +1,49 @@
 var expect = require('chai').expect;
-var snaeks = require('./snaeks').snaeks;
+var sinon = require('sinon');
+
+var Snake = require('./snaeks').Snake;
 
 
-describe("snaek!", function() {
-  it('rocks', function() {
-    expect(snaeks()).to.be.a('string');
+describe("turning", function() {
+  it('always has a direction', function() {
+    var snake = new Snake;
+    expect(snake.direction).not.to.be.undefined;
+  });
+
+  it('turns in directions you tell it', function() {
+    var snake = new Snake;
+    snake.direction = 'up';
+    snake.turn('left');
+    expect(snake.direction).to.equal('left');
+  });
+
+  it('can only go left, right, up, or down', function() {
+    var snake = new Snake;
+    snake.direction = 'down';
+    snake.turn('southwest');
+    expect(snake.direction).to.equal('down');
+  });
+
+  it('can only turn at right angles', function() {
+    var snake = new Snake;
+    snake.direction = 'right';
+    snake.turn('left');
+    expect(snake.direction).to.equal('right');
+  });
+});
+
+
+describe("moving", function() {
+  it('moves one cell in current direction on update', function() {
+    var snake = new Snake;
+    snake.direction = 'right';
+    var init_x = snake.position.x;
+    snake.update();
+    expect(snake.position.x).to.equal(init_x + 1);
+    snake.direction = 'down';
+    var init_y = snake.position.y;
+    snake.update();
+    expect(snake.position.y).to.equal(init_y + 1);
   });
 });
 
--- snaeks.js
+++ snaeks.js
@@ -1,66 +1,71 @@
 if (typeof exports === 'undefined') var exports = window;
 
 
-exports.snaeks = function() {
+exports.snaeks = function(window, document) {
   var board = document.getElementById('gameboard').getContext('2d');
-  board.width = board.canvas.width;
-  board.height = board.canvas.height;
-
-  var snakePosition = {
-    x: 40,
-    y: 40
+  var snake = new Snake;
+  var keyMap = {
+    '37': 'left',
+    '38': 'up',
+    '39': 'right',
+    '40': 'down'
   };
 
-  var direction = 'down';
-
   setInterval(updateGame, 500);
+  window.onkeydown = handleKeydown;
 
   function updateGame() {
     clearBoard();
-    updatePosition();
+    snake.update();
     drawSnake();
   }
 
   function clearBoard() {
     board.fillStyle = "rgb(0,0,0)";
-    board.fillRect(0,0, board.width, board.height);
-  }
-
-  function updatePosition() {
-    switch(direction) {
-      case 'up':
-        snakePosition.y -= 20;
-        break;
-      case 'down':
-        snakePosition.y += 20;
-        break;
-      case 'left':
-        snakePosition.x -= 20;
-        break;
-      case 'right':
-        snakePosition.x += 20;
-        break;
-    }
+    board.fillRect(0, 0, board.canvas.width, board.canvas.height);
   }
 
   function drawSnake() {
     board.fillStyle = "rgb(0,255,0)";
-    board.fillRect(snakePosition.x, snakePosition.y, 20, 20);
+    board.fillRect(snake.position.x * 20, snake.position.y * 20, 20, 20);
   }
 
   function handleKeydown(e) {
-    var keyMap = {
-      '37': 'left',
-      '38': 'up',
-      '39': 'right',
-      '40': 'down'
-    };
-    var key = keyMap[e.which];
-    direction = key || direction;
+    var dir = keyMap[e.which];
+    snake.turn(dir);
   }
+};
 
-  window.onkeydown = handleKeydown;
 
-  return 'whaaaat';
+exports.Snake = function() {
+  var LEFT = 'left';
+  var RIGHT = 'right';
+  var UP = 'up';
+  var DOWN = 'down';
+  var ALL_DIRECTIONS = [LEFT, RIGHT, UP, DOWN];
+  var VERTICAL = [UP, DOWN];
+  var HORIZONTAL = [LEFT, RIGHT];
+
+  this.direction = RIGHT;
+  this.position = {
+    x: 3,
+    y: 3
+  };
+
+  this.update = function() {
+    switch(this.direction) {
+      case LEFT:  this.position.x--; break;
+      case RIGHT: this.position.x++; break;
+      case UP:    this.position.y--; break;
+      case DOWN:  this.position.y++; break;
+    }
+  };
+
+  this.turn = function(newDirection) {
+    if (ALL_DIRECTIONS.indexOf(newDirection) < 0 ||
+        VERTICAL.indexOf(this.direction)+1 && VERTICAL.indexOf(newDirection)+1 ||
+        HORIZONTAL.indexOf(this.direction)+1 && HORIZONTAL.indexOf(newDirection)+1) return;
+    this.direction = newDirection;
+  };
 };
 
