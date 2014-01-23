e9912d06fe2f5eec2fb05554a48f1a4f4bd6bd10
Casey Brant <casey.brant@gmail.com>
2014-01-22 20:47:33 -0800

    snake has a tail, apples pop up all over, snake can eat apples

--- snaek_spec.js
+++ snaek_spec.js
@@ -2,40 +2,39 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 
 var Snake = require('./snaeks').Snake;
+var Apple = require('./snaeks').Apple;
+var GameBoard = require('./snaeks').GameBoard;
 
 
-describe("turning", function() {
-  it('always has a direction', function() {
-    var snake = new Snake;
+describe("Snake", function() {
+  it("always has a direction", function() {
+    var snake = new Snake();
     expect(snake.direction).not.to.be.undefined;
   });
 
-  it('turns in directions you tell it', function() {
-    var snake = new Snake;
+  it("turns in directions you tell it", function() {
+    var snake = new Snake();
     snake.direction = 'up';
     snake.turn('left');
     expect(snake.direction).to.equal('left');
   });
 
-  it('can only go left, right, up, or down', function() {
-    var snake = new Snake;
+  it("can only go left, right, up, or down", function() {
+    var snake = new Snake();
     snake.direction = 'down';
     snake.turn('southwest');
     expect(snake.direction).to.equal('down');
   });
 
-  it('can only turn at right angles', function() {
-    var snake = new Snake;
+  it("can only turn at right angles", function() {
+    var snake = new Snake();
     snake.direction = 'right';
     snake.turn('left');
     expect(snake.direction).to.equal('right');
   });
-});
 
-
-describe("moving", function() {
-  it('moves one cell in current direction on update', function() {
-    var snake = new Snake;
+  it("moves one cell in current direction on update", function() {
+    var snake = new Snake();
     snake.direction = 'right';
     var init_x = snake.position.x;
     snake.update();
@@ -44,6 +43,58 @@ describe("moving", function() {
     var init_y = snake.position.y;
     snake.update();
     expect(snake.position.y).to.equal(init_y + 1);
+  });
+
+  it("grows when it eats", function() {
+    var snake = new Snake();
+    var len = snake.getLength();
+    snake.eat();
+    expect(snake.getLength()).to.equal(len + 1);
+  });
+
+  xit("no specs for the tail? that seems like an oversight...");
+});
+
+
+describe("Apple", function() {
+  it("appears in a random place on the board", function() {
+    sinon.stub(Math, 'random', function rand() {
+      rand.num = rand.num || 0.0;
+      return rand.num += 0.1;
+    });
+    var a1 = new Apple(10, 10);
+    var a2 = new Apple(10, 10);
+    expect(a1.position.x).to.be.lessThan(a2.position.x);
+  });
+});
+
+
+describe("GameBoard", function() {
+  it("keeps the same Apple if the Snake didn't eat it", function() {
+    var board = new GameBoard();
+    board.snake.position = {x:1, y:1};
+    board.apple.position = {x:2, y:2};
+    var originalApple = board.apple;
+    board.update();
+    expect(board.apple).to.equal(originalApple);
+  });
+
+  it("gets a new Apple after the snake eats it", function() {
+    var board = new GameBoard();
+    board.snake.position = {x:1, y:1};
+    board.apple.position = {x:1, y:1};
+    var originalApple = board.apple;
+    board.update();
+    expect(board.apple).not.to.equal(originalApple);
+  });
+
+  it("makes the snake eat the apple if they collide", function() {
+    var board = new GameBoard();
+    board.snake.position = {x:1, y:1};
+    board.apple.position = {x:1, y:1};
+    var snakeEat = sinon.spy(board.snake, 'eat');
+    board.update();
+    expect(snakeEat.called).to.be.true;
   });
 });
 
--- snaeks.js
+++ snaeks.js
@@ -3,21 +3,21 @@ if (typeof exports === 'undefined') var exports = window;
 
 exports.snaeks = function(window, document) {
   var board = document.getElementById('gameboard').getContext('2d');
-  var snake = new Snake;
+  var gameBoard = new GameBoard(board.canvas.width / 20, board.canvas.height / 20);
   var keyMap = {
     '37': 'left',
     '38': 'up',
     '39': 'right',
     '40': 'down'
   };
-
   setInterval(updateGame, 500);
   window.onkeydown = handleKeydown;
 
   function updateGame() {
     clearBoard();
-    snake.update();
+    gameBoard.update();
     drawSnake();
+    drawApple();
   }
 
   function clearBoard() {
@@ -27,12 +27,19 @@ exports.snaeks = function(window, document) {
 
   function drawSnake() {
     board.fillStyle = "rgb(0,255,0)";
-    board.fillRect(snake.position.x * 20, snake.position.y * 20, 20, 20);
+    [gameBoard.snake.position].concat(gameBoard.snake.tail).forEach(function(cell) {
+      board.fillRect(cell.x * 20, cell.y * 20, 20, 20);
+    });
+  }
+
+  function drawApple() {
+    board.fillStyle = "rgb(255,0,0)";
+    board.fillRect(gameBoard.apple.position.x * 20, gameBoard.apple.position.y * 20, 20, 20);
   }
 
   function handleKeydown(e) {
     var dir = keyMap[e.which];
-    snake.turn(dir);
+    gameBoard.snake.turn(dir);
   }
 };
 
@@ -47,17 +54,29 @@ exports.Snake = function() {
   var HORIZONTAL = [LEFT, RIGHT];
 
   this.direction = RIGHT;
-  this.position = {
-    x: 3,
-    y: 3
-  };
+  this.position = {x: 3, y: 3};
+
+  this.tail = [
+    {x: 2, y: 3},
+    {x: 1, y: 3}
+  ];
 
   this.update = function() {
+    this.tail.pop();
+    this.tail.unshift({x: this.position.x, y: this.position.y});
     switch(this.direction) {
-      case LEFT:  this.position.x--; break;
-      case RIGHT: this.position.x++; break;
-      case UP:    this.position.y--; break;
-      case DOWN:  this.position.y++; break;
+      case LEFT:
+        this.position.x--;
+        break;
+      case RIGHT:
+        this.position.x++;
+        break;
+      case UP:
+        this.position.y--;
+        break;
+      case DOWN:
+        this.position.y++;
+        break;
     }
   };
 
@@ -66,6 +85,48 @@ exports.Snake = function() {
         VERTICAL.indexOf(this.direction)+1 && VERTICAL.indexOf(newDirection)+1 ||
         HORIZONTAL.indexOf(this.direction)+1 && HORIZONTAL.indexOf(newDirection)+1) return;
     this.direction = newDirection;
+  };
+
+  this.eat = function() {
+    this.tail.unshift({
+      x: this.position.x,
+      y: this.position.y
+    });
+  };
+
+  this.getLength = function() {
+    return this.tail.length + 1;
+  };
+};
+
+
+exports.Apple = function(boardWidth, boardHeight) {
+  this.position = {
+    x: Math.floor((Math.random() * boardWidth)),
+    y: Math.floor((Math.random() * boardHeight))
+  };
+};
+
+
+exports.GameBoard = function(boardWidth, boardHeight) {
+  this.snake = new exports.Snake();
+  this.apple = new exports.Apple(boardWidth, boardHeight);
+
+  this.update = function() {
+    this.maybeEatApple();
+    this.snake.update();
+  };
+
+  this.maybeEatApple = function() {
+    if (this.collides(this.snake, this.apple)) {
+      this.apple = new exports.Apple(boardWidth, boardHeight);
+      this.snake.eat();
+    }
+  };
+
+  this.collides = function(thing1, thing2) {
+    return thing1.position.x === thing2.position.x &&
+           thing1.position.y === thing2.position.y;
   };
 };
 
