83d1534217ffa96ffba95f33799f2cb7656572fa
Casey Brant <casey.brant@gmail.com>
2014-01-27 19:26:23 -0800

    add minify step to build script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Oh! Snakes!
 
 Dang snakes be *all* up in this.
+
+## To build
+
+1. Run `npm install` to get all the requirements
+2. Run `./build.js` to do a full build and minify
+3. If you are developing, run `./build.js dev` to only concat, not minify
+
--- build.js
+++ build.js
@@ -1,16 +1,38 @@
 #!/usr/local/bin/node
 
 var stitch = require('stitch');
+var minify = require('uglify-js').minify;
 var fs = require('fs');
 
-var package = stitch.createPackage({
-  paths: [__dirname + '/src']
-});
 
-package.compile(function (err, source) {
-  fs.writeFile('snaeks.js', source, function (err) {
-    if (err) throw err;
-    console.log('Compiled snaeks.js');
+var args = process.argv.slice(2);
+if (args.indexOf('dev') >= 0) {
+  concat();
+} else {
+  concat(compress);
+}
+
+
+function concat(callback) {
+  var package = stitch.createPackage({
+    paths: [__dirname + '/src']
   });
-});
+
+  package.compile(function(err, source) {
+    fs.writeFile('snaeks.js', source, function(err) {
+      if (err) throw err;
+      console.log('Compiled snaeks.js');
+      if (callback) callback();
+    });
+  });
+}
+
+function compress(callback) {
+  var minified = minify('snaeks.js');
+  fs.writeFile('snaeks.js', minified.code, function(err) {
+    if (err) throw err;
+    console.log('Minified snaeks.js');
+    if (callback) callback();
+  });
+}
 
